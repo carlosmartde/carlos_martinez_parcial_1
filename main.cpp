cff0ccfa759104eb209be050c556611771465715
carlos humberto martinez de leòn <carlosmartde@gmail.com>
2024-03-02 09:55:34 -0600

    EJERCICIO 2.2 RELIZADO POR CARLOS HUMBERTO MARTINEZ DE LEON CARNET:0909-14-11100

--- main.cpp
+++ main.cpp
@@ -3,22 +3,12 @@ using namespace std;
 
 int main() {
 
-    int a=1;
-    int b=0;
-    int sumapar=0;
-    int sumaimpar=0;
+    int numero=30;
 
-    while (a<=50){
-        sumaimpar+=a;
-        a+=2;
+    for (int i=0; i<=numero; i+=3){
+        cout<<i<<endl;
     }
-        cout<<"LA SUMA DE  LOS NUMEROS IMPARES DE 0 A 50 ES: "<<sumaimpar<<endl;
 
-    while (b<=50){
-        sumapar +=b;
-        b+=2;
-    }
-        cout<<"LA SUMA DE LOS NUMEROS PARES DE 0 A 50 ES: "<<sumapar<<endl;
 
     return 0;
 }
